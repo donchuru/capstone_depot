2ccaf65728f8c0f314b4d4e556c9802325acc21d
donchuru <onchuru@ualberta.ca>
2025-03-01 19:31:10 -0700

    use wsgi.py entry point

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn "capstone_depot:create_app('production')"
+web: gunicorn wsgi:app
 release: flask db upgrade
---
+++ wsgi.py
@@ -0,0 +1,6 @@
+from capstone_depot import create_app
+
+app = create_app('production')
+
+if __name__ == "__main__":
+    app.run()
